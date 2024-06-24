8e1fe9bac32a2bdeaa86147763699792a1dd5569
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-06-25 02:24:54 +0600

    создал проект, добавил preloader

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,11 @@
+import "./Preloader.css";
+
+const Preloader = () => {
+    return (
+        <div id="preloader">
+            <div className="loader">Loading...</div>
+        </div>
+    );
+};
+
+export default Preloader;
